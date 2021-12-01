d1ecd8f3975d04427ba154adad16a15ef51bd641
RomanovGD <95343882+RomanovGD@users.noreply.github.com>
2021-12-01 13:39:00 +0300

    Add files via upload

---
+++ Zadanie_1_1/Zadanie_1_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Zadanie_1_1
+{
+    class Program
+    {
+        static double sum(double fM, double diff, int N)
+        {
+            double lM = fM + (N - 1) * diff;
+            return ((2*fM+diff*(N-1))/2)*N;
+        }
+        static void Main(string[] args)
+        {
+            double firstMember, diff, amount;
+            int count;
+            try
+            {
+                Console.Write("Введите первый член: ");
+                firstMember = Double.Parse(Console.ReadLine());
+                Console.Write("Введите разницу: ");
+                diff = Double.Parse(Console.ReadLine());
+                Console.Write("Введите количество членов: ");
+                count = int.Parse(Console.ReadLine());
+                amount = sum(firstMember, diff, count);
+                Console.WriteLine("Сумма прогрессии: {0}", amount);
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то");
+            }
+        }
+    }
+}
---
+++ Zadanie_1_2/Zadanie_1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadanie_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            try
+            {
+                Console.Write("Введите a: ");
+                a = Double.Parse(Console.ReadLine());
+                Console.Write("Введите b: ");
+                b = Double.Parse(Console.ReadLine());
+                Console.Write("Введите c: ");
+                c = Double.Parse(Console.ReadLine());
+                Console.WriteLine(a==b||a==c||c==b?"Треугольник равнобедренный":"Треугольник не равнобедренный");
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то");
+            }
+        }
+    }
+}
